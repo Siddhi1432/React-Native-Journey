1b7d8b77bec87787f03a80dab670d26ff1e72ef7
Siddhi1432 <siddhirmane14@gmail.com>
2026-05-20 15:36:29 +0530

    Day 7: Built contact list using FlatList

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,11 +1,25 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList, Text } from 'react-native';
 import Greeting from './components/Greetings';
 export default function App() {
+  const friends = [
+    {id:'1',name:'Siddhi'},
+    {id:'2',name:'Rahul'},
+    {id:'3',name:'Neha'},
+    {id:'4',name:'Aarav'},
+    {id:'5',name:'Priya'},
+    {id:'6',name:'Sandesh'},
+    {id:'7',name:'Riya'},
+    {id:'8',name:'Pooja'},
+  ];
   return (
     <View style={styles.container}>
-      <Greeting name="Siddhi" />
-      <Greeting name="React Native" />
-      <Greeting name="Future Full Stack Developer" />
+      
+      <Text style={styles.title}>My Friends List</Text>
+
+      <FlatList data={friends}
+      renderItem={({item}) => (
+        <Text style={styles.item}>{item.name}</Text>
+      )}/>
     </View>
   );
 }
@@ -13,7 +27,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    justifyContent:"center",
-    alignItems:"center"
+    paddingTop: 60,
+    paddingHorizontal: 20
+  },
+  title:{
+    fontSize:25,
+    fontWeight:'bold',
+    marginBottom:20
+  },
+
+  item:{
+    fontSize:20,
+    padding:15,
+    marginBottom:10,
+    borderWidth:1
   }
 });
